7af4b25a03f03770c0c96aa8b9e84d88f1248ba7
Md. Al Amin <48113312+alaminhossain171@users.noreply.github.com>
2021-12-02 17:43:02 +0600

    main dashboard layout and navigation bar fully completed

--- src/Components/Pages/MainDashbord/MainDashbord.js
+++ src/Components/Pages/MainDashbord/MainDashbord.js
@@ -3,12 +3,14 @@ import Card1LeftsideBar from "./components/Card1LeftsideBar";
 import Card2LeftsideBar from "./components/Card2LeftsideBar";
 import bank1 from "../../../assets/images/RightSidebar/1.png";
 import bank2 from "../../../assets/images/RightSidebar/2.jpg";
-import graph from "../../../assets/images/RightSidebar/canvas 1.png";
+// import graph from "../../../assets/images/RightSidebar/canvas 1.png";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 
 import "./MainDashbord.css";
+import MonthCard from "./components/MonthCard";
+import HistoryCard from "./components/HistoryCard";
 const MainDashbord = () => {
   return (
     <div className="custom-body">
@@ -20,21 +22,22 @@ const MainDashbord = () => {
           </div>
 
           <div className="col-md-8 ">
+              {/* Balancea and transfer card start here */}
             <div className="row">
               <div className="col-md-12 col-lg-4">
-                <div class="card bg-dark my-3 card-dashbord">
-                  <div class="card-body mb-2">
+                <div className="card bg-dark my-3 card-dashbord">
+                  <div className="card-body mb-2">
                     <h5 className="text-light">Balance</h5>
                     <h2 className="text-warning">$ 14,500</h2>
-                    <span class="card-link text-light">
+                    <span className="card-link text-light">
                       <span>
-                        <i class="fas fa-arrow-up mx-2 p-1 bg-secondary"></i>
+                        <i className="fas fa-arrow-up mx-2 p-1 bg-secondary"></i>
                       </span>
                       +3200.0
                     </span>
-                    <span class="card-link text-light">
+                    <span className="card-link text-light">
                       <span>
-                        <i class="fas fa-arrow-down mx-2 p-1 bg-secondary"></i>
+                        <i className="fas fa-arrow-down mx-2 p-1 bg-secondary"></i>
                       </span>
                       -0.000
                     </span>
@@ -43,8 +46,8 @@ const MainDashbord = () => {
               </div>
 
               <div className="col-md-12 col-lg-8">
-                <div class="card bg-dark my-3 card-dashbord">
-                  <div class="card-body">
+                <div className="card bg-dark my-3 card-dashbord">
+                  <div className="card-body">
                     <h5 className="text-light">Transfer to</h5>
                     <OwlCarousel
                       className="owl-theme"
@@ -53,7 +56,7 @@ const MainDashbord = () => {
                       items={6}
                       nav
                     >
-                      <div class="item">
+                      <div className="item">
                         <div className="imgItem">
                           <img
                             src={bank1}
@@ -62,7 +65,7 @@ const MainDashbord = () => {
                           />
                         </div>
                       </div>
-                      <div class="item">
+                      <div className="item">
                         <div className="imgItem">
                           <img
                             src={bank2}
@@ -71,7 +74,7 @@ const MainDashbord = () => {
                           />
                         </div>
                       </div>
-                      <div class="item">
+                      <div className="item">
                         <div className="imgItem">
                           <img
                             src={bank1}
@@ -80,7 +83,7 @@ const MainDashbord = () => {
                           />
                         </div>
                       </div>
-                      <div class="item">
+                      <div className="item">
                         <div className="imgItem">
                           <img
                             src={bank2}
@@ -94,96 +97,21 @@ const MainDashbord = () => {
                 </div>
               </div>
             </div>
+      {/* Balancea and transfer card start here */}
 
+ {/* graph and History card start here
+  */}
             <div className="row my-4">
               <div className="col-md-12 col-lg-8 mb-3">
-                <div class="card card-dashbord bg-dark">
-                  <div class="card-body">
-                    <h5 className="text-light">Monthly Earning</h5>
-                    <div className="text-center">
-                      <img src={graph} className="img-fluid" alt="" />
-                      <button className="btn btn-warning my-3 fw-bold px-4">
-                        Details
-                      </button>
-                    </div>
-                  </div>
-                </div>
+              <MonthCard />
               </div>
 
               <div className="col-md-12 col-lg-4 ">
-                <div class="card card-dashbord bg-dark">
-                  <div class="card-body">
-                    <h5 className="text-light">History</h5>
-                    <div className="loopCard mx-auto">
-                      <div class="card money-card">
-                        <div class="container d-flex justify-content-between">
-                          <small className="text-light my-1 pending">
-                            Pending
-                          </small>
-                          <p className="text-light my-1 ">2:20 pm</p>
-                        </div>
-                        <div class="container d-flex justify-content-between">
-                          <h4 className="text-warning">$14,500</h4>
-                          <p className="text-light">21-10-2021</p>
-                        </div>
-                      </div>
-
-                      <div class="card money-card my-3">
-                        <div class="container d-flex justify-content-between">
-                          <small className="text-light my-1 transferred">
-                            Transferred
-                          </small>
-                          <p className="text-light my-1 ">2:20 pm</p>
-                        </div>
-                        <div class="container d-flex justify-content-between">
-                          <h4 className="text-warning">$14,500</h4>
-                          <p className="text-light">21-10-2021</p>
-                        </div>
-                      </div>
-
-                      <div class="card money-card my-3">
-                        <div class="container d-flex justify-content-between">
-                          <small className="text-light my-1 transferred">
-                            Transferred
-                          </small>
-                          <p className="text-light my-1 ">2:20 pm</p>
-                        </div>
-                        <div class="container d-flex justify-content-between">
-                          <h4 className="text-warning">$14,500</h4>
-                          <p className="text-light">21-10-2021</p>
-                        </div>
-                      </div>
-
-                      <div class="card money-card my-3">
-                        <div class="container d-flex justify-content-between">
-                          <small className="text-light my-1 transferred">
-                            Transferred
-                          </small>
-                          <p className="text-light my-1 ">2:20 pm</p>
-                        </div>
-                        <div class="container d-flex justify-content-between">
-                          <h4 className="text-warning">$14,500</h4>
-                          <p className="text-light">21-10-2021</p>
-                        </div>
-                      </div>
-
-                      <div class="card money-card my-3">
-                        <div class="container d-flex justify-content-between">
-                          <small className="text-light my-1 transferred">
-                            Transferred
-                          </small>
-                          <p className="text-light my-1 ">2:20 pm</p>
-                        </div>
-                        <div class="container d-flex justify-content-between">
-                          <h4 className="text-warning">$14,500</h4>
-                          <p className="text-light">21-10-2021</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+               <HistoryCard />
               </div>
             </div>
+             {/* graph and History card end here
+  */}
           </div>
         </div>
       </div>
