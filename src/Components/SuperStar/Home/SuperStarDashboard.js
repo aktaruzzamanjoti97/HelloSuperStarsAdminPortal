94b739bdf145a3f608dd7732d9c6ca30e62308e7
M. Aktaruzzaman Joti <63288034+aktaruzzamanjoti97@users.noreply.github.com>
2021-12-14 14:59:48 +0600

    Textarea  summernote

--- src/Components/SuperStar/Home/SuperStarDashboard.js
+++ src/Components/SuperStar/Home/SuperStarDashboard.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import AdminMainDashboard from '../../Pages/MainDashbord/AdminMainDashboard'
+import MainDashbord from '../../Pages/MainDashbord/MainDashbord'
 const SuperStarDashboard = () => {
     return (
         <>
-             <h1 style={{ color:"tomato" }}>SuperStar DashBoard</h1>
+             {/* <h1 style={{ color:"tomato" }}>SuperStar DashBoard</h1> */}
+             <AdminMainDashboard/>
         </>
     )
 }
