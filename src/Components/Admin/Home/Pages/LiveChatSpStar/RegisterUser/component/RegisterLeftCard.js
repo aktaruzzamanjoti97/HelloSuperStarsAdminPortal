395f4355f372a330831ce87b5da3c4c83abeba91
Md. Shawn Shikder Sonet <75745559+sonet1182@users.noreply.github.com>
2021-12-14 20:13:56 +0600

    Update RegisterLeftCard.js

--- src/Components/Admin/Home/Pages/LiveChatSpStar/RegisterUser/component/RegisterLeftCard.js
+++ src/Components/Admin/Home/Pages/LiveChatSpStar/RegisterUser/component/RegisterLeftCard.js
@@ -44,11 +44,8 @@ export default function RegisterLeftCard(props) {
         <div className="card-body">
           <h5 className="card-title text-warning">Instruction</h5>
           <p className="card-text text-light">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
-            minus inventore? Libero asperiores aliquid dolorum?
+            {liveChat.description}
           </p>
-          <p className="text-light">Lorem ipsum dolor sit amet.</p>
-
           <div className="time-table d-flex  ">
             <span className="clock">
               <i className="fas fa-calendar-week fa-3x p-2"></i>
@@ -78,7 +75,7 @@ export default function RegisterLeftCard(props) {
             <div className="other mx-1 p-2">
               <span className="text-light">Cost per minute</span>
               <br></br>
-              <span className="text-warning date-font">1200 BDT</span>
+              <span className="text-warning date-font">{liveChat.fee} BDT</span>
             </div>
           </div>
         </div>
