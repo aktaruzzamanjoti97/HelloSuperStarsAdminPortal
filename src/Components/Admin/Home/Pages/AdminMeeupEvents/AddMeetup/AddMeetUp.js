5aa78918d92591e9b377a5c587b50125aaf54cdf
M. Aktaruzzaman Joti <63288034+aktaruzzamanjoti97@users.noreply.github.com>
2021-12-30 18:16:47 +0600

    Add Meetup form and Live meetup completed

---
+++ src/Components/Admin/Home/Pages/AdminMeeupEvents/AddMeetup/AddMeetUp.js
@@ -0,0 +1,251 @@
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import MeetUpEventNave from '../MeetUpEventNave';
+import './AddMeetUp.css';
+
+
+
+const AddMeetUp = () => {
+
+    const [activity, setActivity] = React.useState('');
+
+    const handleChange = (event) => {
+        setActivity(event.target.value);
+
+
+
+    };
+
+    return (
+        <>
+            <div className="AS">
+                {/* <MeetUpEventNave /> */}
+
+                <div className="greetingsFormBorder">
+                    <div className="greetingsPadding addMeetUpPadding">
+                        <div className="d-flex">
+                            <div className="addIconGreetings">
+                                <i style={{ color: '#ffc107' }} class="far fa-plus-square mx-2"></i>
+                            </div>
+                            <h3 className="text-warning text-bold">Create Meetup</h3>
+                        </div>
+                        <form action="">
+                            <div className="row my-4">
+                                <div className="d-flex col-md-5 text-white eventType">
+                                    {/* <label className='eventType' for="selectOnlineOffline"><big>Event Type</big></label>
+                                    <select className="input-gray" name="online-offline" id="selectOnlineOffline">
+                                        <option value="online">Online</option>
+                                        <option value="offline">Offline</option>
+                                    </select> */}
+
+                                    {/* <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value={age}
+                                        label="Age"
+                                        onChange={handleChange}
+                                    >
+                                        <MenuItem value={10}>Ten</MenuItem>
+                                        <MenuItem value={20}>Twenty</MenuItem>
+                                        <MenuItem value={30}>Thirty</MenuItem>
+                                    </Select> */}
+
+                                    <div className="row">
+                                        <div className="col-md-3">
+                                            <p className=""><big>Event type</big></p>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <Box className="mx-3 selectActivity" sx={{ minWidth: 300 }}>
+                                                <FormControl fullWidth>
+                                                    <InputLabel id="demo-simple-select-label text-white">Set Activity</InputLabel>
+                                                    <Select
+                                                        labelId="demo-simple-select-label"
+                                                        id="demo-simple-select"
+                                                        value={activity}
+                                                        label="Age"
+                                                        onChange={handleChange}
+                                                    >
+                                                        <MenuItem value={10}>Online</MenuItem>
+                                                        <MenuItem value={20}>Offline</MenuItem>
+
+                                                    </Select>
+                                                </FormControl>
+                                            </Box>
+                                        </div>
+                                    </div>
+
+
+
+
+
+
+
+                                </div>
+                                <div className="col-md-7 px-3">
+                                    <div className="d-flex">
+                                        <div className="col-md-2 text-white">
+                                            <p><big>Event name</big></p>
+                                        </div>
+                                        <input className="form-control input-gray" type="text" />
+                                    </div>
+
+
+
+                                </div>
+                            </div>
+
+                            <div className="row my-4">
+                                <div className="col-md-1 text-white">
+                                    <p><big>Description</big></p>
+                                </div>
+                                <div className="col-md-11">
+                                    <textarea className="form-control input-gray me-4" type="text" />
+                                </div>
+                            </div>
+
+                            <div className="row my-4">
+                                <div className="col-md-4">
+                                    <div className="row">
+                                        <div className="col-md-4">
+                                            <p className="text-white"><big>Date & Time</big></p>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <input className="form-control input-gray text-white" type="date" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-8">
+                                    <div className="row">
+                                        <div className="col-md-6">
+                                            <div className="row">
+                                                <div className="col-md-2">
+                                                    <p className="text-white"><big>From</big></p>
+                                                </div>
+                                                <div className="col-md-6">
+                                                    <input className="form-control input-gray text-white" type="time" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <div className="row">
+                                                <div className="col-md-2">
+                                                    <p className="text-white"><big>To</big></p>
+                                                </div>
+                                                <div className="col-md-6">
+                                                    <input className="form-control input-gray text-white" type="time" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {
+                                activity === 10 ? null : (
+                                    <>
+                                        <div className="row my-4">
+                                            <div className="col-md-1 text-white">
+                                                <p><big>Venue</big></p>
+                                            </div>
+                                            <div className="col-md-11">
+                                                <input className="form-control input-gray me-4" type="text" />
+                                            </div>
+                                        </div>
+
+                                        <div className="row my-4">
+                                            <div className="col-md-1 text-white">
+                                                <p><big>Slots</big></p>
+                                            </div>
+                                            <div className="col-md-2">
+                                                {/* <textarea className="form-control input-gray me-4" type="text" /> */}
+                                                <input className="form-control input-gray" type="number" />
+                                            </div>
+                                        </div>
+                                    </>
+                                )
+                            }
+
+                            {
+                                activity === 10 ? (
+                                    <>
+                                                            <div className="row my-4">
+                        <div className="col-md-2 text-white">
+                            <p><big>Upload Banner</big></p>
+                        </div>
+                        <div className="col-md-10">
+                            <input type="file" name="file" id="file" className="inputfile" />
+                            <label for="file"><i class="fas fa-cloud-upload-alt"></i> Upload</label>
+
+                        </div>
+                    </div>
+                    <div className="row my-4">
+                        <div className="col-md-2 text-white">
+                            <p><big>Upload Video</big></p>
+                        </div>
+                        <div className="col-md-10">
+                            <input type="file" name="file" id="file" className="inputfile" />
+                            <label for="file"><i class="fas fa-cloud-upload-alt"></i> Upload</label>
+                        </div>
+                    </div>
+                                    </>
+                                ) : 
+                                (
+                                    <div className="row my-4">
+                                <div className="col-md-4">
+                                    <div className="row">
+                                        <div className="col-md-4 text-white">
+                                            <p><big>Upload Banner</big></p>
+                                        </div>
+                                        <div className="col-md-5">
+                                            <input type="file" name="file" id="file" className="inputfile" />
+                                            <label for="file"><i class="fas fa-cloud-upload-alt"></i> Upload</label>
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="row">
+                                        <div className="col-md-4 text-white">
+                                            <p><big>Upload Video</big></p>
+                                        </div>
+                                        <div className="col-md-5">
+                                            <input type="file" name="file" id="file" className="inputfile" />
+                                            <label for="file"><i class="fas fa-cloud-upload-alt"></i> Upload</label>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                                )
+                            }
+
+                          
+
+
+                            
+
+                            <div className="my-3">
+                                <Link to="/superstar-admin/greetings/details">
+                                    <button className="btn btn-warning save-greetings-button py-2"><big><b>Confirm</b></big></button>
+                                </Link>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default AddMeetUp
---
+++ src/Components/Admin/Home/Pages/AdminMeeupEvents/LiveMeetUp/CompletedEventSliders/CompletedEventSliders.js
@@ -0,0 +1,218 @@
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import Slider from 'react-slick';
+import MeetupImage1 from '../../../../../../../assets/images/MeetupImages/unsplash_MXbM1NrRqtI.png';
+import RadisonBlue from '../../../../../../../assets/images/MeetupImages/unsplash_QdAAasrZhdk.png';
+import LeMerridian from '../../../../../../../assets/images/MeetupImages/unsplash_xEaAoizNFV8.png';
+import Session from '../../../../../../../assets/images/MeetupImages/unsplash_YC8qqp50BdA.png'
+import EnterImage from '../../../../../../../assets/images/enter 1.png'
+import './CompletedEventSliders.css';
+
+const CompletedEventSliders = () => {
+
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
+    return (
+        <div>
+
+            <div className="slick-parent d-flex justify-content-center">
+                <Slider className="slider-width" {...settings}>
+                    <div className="p-3">
+                        <div className="completedMeetupBlack">
+                            <img src={MeetupImage1} className="img-fluid" alt="" />
+                            <div className="p-3">
+                                <div className="d-flex justify-content-between">
+                                    <h5 className="text-white">Friday Evening with SAH75</h5>
+                                    <img src={EnterImage} alt="" />
+                                </div>
+
+                                <p className="text-secondary">Lorem Ipsum is simply dummy text of the printing and
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="p-3">
+                        <div className=" completedMeetupBlack">
+                            <img src={Session} className="img-fluid" alt="" />
+                            <div className="p-3">
+                                <div className="d-flex justify-content-between">
+                                    <h5 className='text-white'>Raddusson Blue</h5>
+                                    <img src={EnterImage} alt="" />
+                                </div>
+
+                                <p className='text-secondary'>Lorem Ipsum is simply dummy text of the printing and
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="p-3">
+                        <div className=" completedMeetupBlack">
+                            <img src={LeMerridian} className="img-fluid" alt="" />
+                            <div className="p-3">
+                                <div className="d-flex justify-content-between">
+                                    <h5 className='text-white'>Le Merridian 11.11</h5>
+                                    <img src={EnterImage} alt="" />
+                                </div>
+
+                                <p className='text-secondary'>Lorem Ipsum is simply dummy text of the printing and
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="p-3">
+                        <div className=" completedMeetupBlack">
+                            <img src={Session} className="img-fluid" alt="" />
+                            <div className="p-3">
+                                <div className="d-flex justify-content-between">
+                                    <h5 className='text-white'>Friday Evening with SAH75</h5>
+                                    <img src={EnterImage} alt="" />
+                                </div>
+
+                                <p className='text-secondary'>Lorem Ipsum is simply dummy text of the printing and
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="p-3">
+                        <div className=" completedMeetupBlack">
+                            <img src={Session} className="img-fluid" alt="" />
+                            <div className="p-3">
+                                <div className="d-flex justify-content-between">
+                                    <h5>Session Moneyheist</h5>
+                                    <img src={EnterImage} alt="" />
+                                </div>
+
+                                <p className='text-secondary'>Lorem Ipsum is simply dummy text of the printing and
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="p-3">
+                        <div className=" completedMeetupBlack">
+                            <img src={MeetupImage1} className="img-fluid" alt="" />
+                            <div className="p-3">
+                                <div className="d-flex justify-content-between">
+                                    <h5>Friday Evening with SAH75</h5>
+                                    <img src={EnterImage} alt="" />
+                                </div>
+
+                                <p className='text-secondary'>Lorem Ipsum is simply dummy text of the printing and
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="p-3">
+                        <div className=" completedMeetupBlack">
+                            <img src={MeetupImage1} className="img-fluid" alt="" />
+                            <div className="p-3">
+                                <div className="d-flex justify-content-between">
+                                    <h5>Friday Evening with SAH75</h5>
+                                    <img src={EnterImage} alt="" />
+                                </div>
+
+                                <p className='text-secondary'>Lorem Ipsum is simply dummy text of the printing and
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="p-3">
+                        <div className=" completedMeetupBlack">
+                            <img src={MeetupImage1} className="img-fluid" alt="" />
+                            <div className="p-3">
+                                <div className="d-flex justify-content-between">
+                                    <h5 className="text-white">Friday Evening with SAH75</h5>
+                                    <img src={EnterImage} alt="" />
+                                </div>
+
+                                <p className='text-secondary'>Lorem Ipsum is simply dummy text of the printing and
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </Slider>
+            </div>
+
+
+            {/* <Slider className="mx-2 slickCardWidth" {...settings}>
+                <Card className="slickCard">
+                    <Card.Img className="img-fluid" variant="top" src={MeetupImage1} />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+
+                <Card className="slickCard">
+                    <Card.Img className="img-fluid" variant="top" src={MeetupImage1} />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+
+             
+
+                
+            </Slider> */}
+        </div>
+    );
+};
+
+export default CompletedEventSliders;
