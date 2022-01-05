96fdde53322484a0a25a9c2ec32b625a6777f46e
Apurbo Kumar Anup <apurboka@gmail.com>
2022-01-05 17:28:35 +0600

    Cricket Jersey, Souvenir Navbar, Live Now

--- src/Components/Admin/Home/Pages/AdminSouvenir/Content/CricketJersey/CricketJerseySouvenir.js
+++ src/Components/Admin/Home/Pages/AdminSouvenir/Content/CricketJersey/CricketJerseySouvenir.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Fly from '../../../../../../../assets/images/Souvenir/unsplash_Z4RYz52ljts.png'
 import calendar from '../../../../../../../assets/images/meetupEvent/calendar.png'
 import Clock from '../../../../../../../assets/images/meetupEvent/clock-icon.png'
-
+import './CricketJer.css'
 const CricketJerseySouvenir = () => {
 return (
 <div>
     <div className="row my-3">
-        <div className="col-md-8 mb-4 ">
+        <div className="col-md-8 mb-5 ">
             <div className="card event-card2 shadow">
                 <div className="card-body">
                     <h5 className="text-light mb-3">
@@ -25,39 +25,37 @@ return (
                 </div>
             </div>
 
-            <div className="mt-5 mb-5">
-                <div className="d-flex">
-                    <div className="d-flex p-2 buTon-EMN">
-                        <div className="mt-2  mx-3 ">
-                            <span className="text-light"> <img src={calendar} alt="" className='PriceTage' /></span>
-                        </div>
-                        <div className=" mx-1">
-                            <span className="text-light">Date</span>
-                            <br></br>
-                            <span className="text-warning BtnTextNMP fw-bold">10:00 PM - 11:00 PM</span>
-                        </div>
+            <div className=" d-flex mt-5 mb-5 cricket-jersey">
+                <div className="d-flex p-2 buTon-EMN">
+                    <div className="mt-2  mx-3 ">
+                        <span className="text-light"> <img src={calendar} alt="" className='PriceTage' /></span>
                     </div>
+                    <div className=" mx-1">
+                        <span className="text-light">Date</span>
+                        <br></br>
+                        <span className="text-warning BtnTextNMP fw-bold">10:00 PM - 11:00 PM</span>
+                    </div>
+                </div>
 
-                    <div className=" d-flex p-2 buTon-EMN ">
-                        <div className=" mt-2 mx-3">
-                            <span className="text-light"> <img src={Clock} alt="" className='PriceTage' /></span>
-                        </div>
-                        <div className=" mx-1 ">
-                            <span className="text-light">Time</span>
-                            <br></br>
-                            <span className="text-warning BtnTextNMP fw-bold">10:00 PM - 11:00 PM</span>
-                        </div>
+                <div className=" d-flex p-2 buTon-EMN ">
+                    <div className=" mt-2 mx-3">
+                        <span className="text-light"> <img src={Clock} alt="" className='PriceTage' /></span>
+                    </div>
+                    <div className=" mx-1 ">
+                        <span className="text-light">Time</span>
+                        <br></br>
+                        <span className="text-warning  BtnTextNMP fw-bold">10:00 PM - 11:00 PM</span>
                     </div>
                 </div>
             </div>
 
-            <button className='btn MEN-X fw-bold'>Approve </button>
-            <button className='btn MEN-Y text-warning fw-bold mx-3'>Decline</button>
+            <button className='btn Cri-btn-x border-warning fw-bold px-5'>Approve </button>
+            <button className='btn border-warning fw-bold mx-3  px-5 Cri-btn'>Edit</button>
 
         </div>
 
-        <div className="col-md-4">
-            <img src={Fly} alt="Pineapple" className='img-fluid' />
+        <div className="col-md-4 ">
+            <img src={Fly} alt="Pineapple" className='img-fluid Cricket-Img' />
         </div>
 
     </div>
