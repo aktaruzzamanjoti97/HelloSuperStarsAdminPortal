d000b18e5f9ae346ccbeb89d4b60d09e7276e165
Md. Shawn Shikder Sonet <75745559+sonet1182@users.noreply.github.com>
2022-01-18 12:12:36 +0600

    Approve

--- src/Components/SuperStar/Pages/StarLiveChat/RegisterUser/StarRegistertUser.js
+++ src/Components/SuperStar/Pages/StarLiveChat/RegisterUser/StarRegistertUser.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from "react";
-import "./RegisterUser.css";
+import "./StarRegistertUser.css";
 import fatema from "../../../../../assets/images/AdminLiveProfile/cx.webp";
 import enjoy from '../../../../../assets/images/AdminLiveProfile/abc.jpg';
 import axios from "axios";
 import { useHistory} from 'react-router-dom';
 import moment from 'moment';
 import Iframe from 'react-iframe';
+import ReactPlayer from "react-player";
 
 import StarRegisterLeftCard from "./component/StarRegisterLeftCard";
+import { Markup } from "interweave";
 
 const StarRegisterUser = (props) => {
   const [showItem, setShowItem] = useState(false);
   const [registeredLiveChat, setRegisteredLiveChat] = useState([]);
+  const [event, setEvent] = useState('');
 
   const history = useHistory();
   let img_src = '';
@@ -23,7 +26,7 @@ const StarRegisterUser = (props) => {
   useEffect(() => {
     let isMounted = true;
 
-    const live_chat_id = props.match.params.live_chat_id;
+    const live_chat_id = props.match.params.id;
 
     axios.get(`/api/registeredUserList/${live_chat_id}`).then(res =>{
 
@@ -32,12 +35,13 @@ const StarRegisterUser = (props) => {
             if(res.data.status === 200)
             {
               setRegisteredLiveChat(res.data.registeredLiveChats);
-              // console.log(res.data.registeredLiveChats);
+              setEvent(res.data.event);
+              console.log(res.data.event);
             }
         }           
     });
 
-}, [props.match.params.star_id, history]);
+}, [props.match.params.id, history]);
 
 
 var viewRegisteredUserList = null
@@ -54,7 +58,7 @@ registeredLiveChat.map( (item, index) => {
     <tr>
       <th scope="row">
         <img src={img_src} alt="imgBaseURL" className="rounded-circle register-user-img" />
-        <span> {item.user.first_name} {item.user.last_name}</span>
+        <span> {item.user?.first_name} {item.user?.last_name}</span>
       </th>
       <th>{moment(item.live_chat_start_time).format('h:mm A')}</th>
       <th>{Math.abs(new Date(item.live_chat_end_time)  - new Date(item.live_chat_start_time)) / 60000} Minute(s)</th>
@@ -100,8 +104,59 @@ registeredLiveChat.map( (item, index) => {
 
         </div>
         ) : (
-          <StarRegisterLeftCard data={props.match.params.live_chat_id} />
+          <div className="card right-card-live mx-auto my-2">
+        {/* <img className="card-img-top" src={sakib} alt="sakib img"  />
+         */}
+        <ReactPlayer
+          url="https://youtu.be/dgfTiONcnTc"
+          playing={false}
+          volume={1}
+          width="100%"
+          height="320px"
+          style={{ margin: "0 auto" }}
+        />
+
+        <div className="card-body">
+          <h5 className="card-title text-warning">Instruction</h5>
+          <p className="card-text text-light">
+            <Markup content={event.description}/>
+          </p>
+          <div className="time-table d-flex  ">
+            <span className="clock">
+              <i className="fas fa-calendar-week fa-3x p-2"></i>
+            </span>
+            <div className="other mx-1 p-2">
+              <span className="text-light">{event.title}</span>
+              <br></br>
+              <span className="text-warning date-font">{moment(event.date).format('LL')}</span>
+            </div>
+          </div>
+
+          <div className="time-table2 d-flex  my-3">
+            <span className="clock">
+              <i className="fas fa-clock  fa-3x p-2"></i>
+            </span>
+            <div className="other mx-1 p-2">
+              <span className="text-light">Time</span>
+              <br></br>
+              <span className="text-warning date-font">{moment(event.start_time).format('h:mm A')}-{moment(event.end_time).format('h:mm A')}</span>
+            </div>
+          </div>
+
+          <div className="time-table3 d-flex  my-3">
+            <span className="clock">
+              <i className="fas fa-dollar-sign fa-3x p-2"></i>
+            </span>
+            <div className="other mx-1 p-2">
+              <span className="text-light">Cost per minute</span>
+              <br></br>
+              <span className="text-warning date-font">{event.fee} BDT</span>
+            </div>
+          </div>
+        </div>
+      </div>
         )}
+        
       </div>
     </div>
   );
--- src/Components/SuperStar/Pages/StarLiveChat/StarAddSessionContent.js
+++ src/Components/SuperStar/Pages/StarLiveChat/StarAddSessionContent.js
@@ -4,71 +4,13 @@ import Live from '../../../../assets/images/instagram-live 1.png'
 import Pending from '../../../../assets/images/pending 2.png'
 import Add from '../../../../assets/images/UpcomingEvent/add 1.png'
 import AddSession from './Content/StarAddSession'
+import Nav from './Nav'
 
 const AddSessionContent = () => {
 return (
 <>
     <div className="AS">
-        <div className="mb-3 row">
-
-            <div title="Live Now" className="col-md-3 align-items-center justify-content-center ">
-                <div className="card ad-card SH-CARD-TSX">
-                    <div className="card-body  align-items-center">
-                        <center>
-                            <tr>
-                                <td className="ad-card-td">
-                                    <img src={Live} className="ad-card-img-top" alt="..." />
-                                </td>
-                                <td className="ad-card-td">
-                                    <small className="ad-card-small">01</small>
-                                </td>
-                            </tr>
-                        </center>
-                    </div>
-                    <Link to='/superstar/live-chat/' className="card-footer ad-card-footer"><button className="ad-card-footer" data-bs-toggle="collapse"
-                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Live Now
-                    </button></Link>
-                </div>
-            </div><br/>
-            <div title="Live Now" className="col-md-3 align-items-center justify-content-center ">
-                <div className="card ad-card SH-CARD-TSX">
-                    <div className="card-body  align-items-center">
-                        <center>
-                            <tr>
-                                <td className="ad-card-td">
-                                    <img src={Pending} className="ad-card-img-top" alt="..." />
-                                </td>
-                                <td className="ad-card-td">
-                                    <small className="ad-card-small">01</small>
-                                </td>
-                            </tr>
-                        </center>
-                    </div>
-                    <Link to='/superstar/live-chat/pending-session' className="ad-card-footer"><button className="card-footer ad-card-footer" data-bs-toggle="collapse"
-                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Pending
-                    </button></Link>
-                </div>
-            </div><br/>
-            <div title="Live Now" className="col-md-3 align-items-center justify-content-center ">
-                <div className="card ad-card SH-CARD-TSX">
-                    <div className="card-body  align-items-center">
-                        <center>
-                            <tr>
-                                <td className="ad-card-td">
-                                    <img src={Add} className="ad-card-img-top" alt="..." />
-                                </td>
-                                <td className="ad-card-td">
-                                    <small className="ad-card-small"></small>
-                                </td>
-                            </tr>
-                        </center>
-                    </div>
-                    <Link to='/superstar/live-chat/add-session' className="ad-card-footer"><button className="card-footer ad-card-footer" data-bs-toggle="collapse"
-                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> Add Session
-                    </button></Link>
-                </div>
-            </div>
-        </div>
+        <Nav></Nav>
         <AddSession/>
     </div>
 </>
