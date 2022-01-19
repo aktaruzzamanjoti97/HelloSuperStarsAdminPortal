d0992d17a8d5e3a8d3325cf80d8b312b6f9b8f94
M. Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2022-01-19 13:53:34 +0600

    Live chat schedule add remove completed

--- src/Components/Admin/Home/Pages/LiveChatSpStar/LiveChatSchedule.js
+++ src/Components/Admin/Home/Pages/LiveChatSpStar/LiveChatSchedule.js
@@ -10,16 +10,30 @@ const LiveChatSchedule = () => {
     const singleInput = (
         <LiveChatScheduleFormCom />
     );
+
+    const handleAddClick = () => {
+        setData([...data, singleInput]);
+    }
+
+
     return (
         <div className='container-fluid my-2'>
             <div className="d-flex justify-content-between">
                 <h5 className='text-light'>Schedules</h5>
-                <button onClick={() => {
-                    setData([...data, singleInput]);
-                }} className="btn btn-warning px-4 py-1"><span className='fw-bold'>+</span> New Schedule</button>
+
+                <div className="d-flex flex-column">
+                    <button onClick={handleAddClick} type="button" className="btn btn-warning px-4 py-1"><span className='fw-bold'>+</span> New Schedule</button>
+                </div>
+
             </div>
-            {data.map((single,index) => {
-                return <div key={index}>{single}</div>;
+            {data.map((single, i) => {
+                return (
+                    <div>
+
+                        <div key={i}>{single}</div>
+                        <div>{console.log(single)}</div>
+                    </div>
+                );
             })}
         </div>
     );
