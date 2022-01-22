c6a60fc6c53169bdaa02db0c5ed1deb33ec2177d
Apurbo Kumar Anup <apurboka@gmail.com>
2022-01-22 17:20:24 +0600

    Approved

---
+++ src/Components/SuperStar/Pages/StarLearningSession/Content/Approved/ApprovedLearnEvent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ApprovedLearnEvent = () => {
+return(
+<>
+    <div className="col-md-12 ">
+        <div className="d-flex pt-3">
+            <div className="faTrophy"><i class="fas fa-trophy"></i></div>
+            <h4 className="mx-3 text-light p-2">Approved Learning Session ( Event )</h4>
+        </div>
+
+        Write hre.....
+
+
+
+    </div>
+</>
+)
+};
+
+export default ApprovedLearnEvent;
---
+++ src/Components/SuperStar/Pages/StarLearningSession/Content/Request/RequestLearn.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import LearnSessionNaveStar from '../../LearnSessionNaveStar'
+import RequestLearnSlide from './RequestLearnSlide'
+
+const RequestLearn = () => {
+  return (
+    <>
+    <div className="AS">
+        <LearnSessionNaveStar />
+
+        <div className="liveMeetupBorder my-5">
+            <div className="d-flex px-4 pt-3">
+                <div className="faTrophy"><i class="fas fa-trophy"></i></div>
+                <h4 className="mx-3 text-white p-2">Requested Learning Session</h4>
+            </div>
+
+            <div className="pb-3">
+                <RequestLearnSlide/>
+            </div>
+            <center><Link to='/superstar/learning-session/request-all'><Button className='btn bg-warning mt-3 mb-3 fw-bold text-dark ' > View All</Button></Link></center>
+
+        </div>
+       
+    </div>
+      
+    </>
+  )
+}
+
+export default RequestLearn
