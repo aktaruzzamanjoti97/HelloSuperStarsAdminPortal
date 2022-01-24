5af978ce0274800e01fa5d10391b203d682266d7
M. Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2022-01-24 14:59:55 +0600

    scheule card added

--- src/Components/Admin/Home/Pages/LiveChatSpStar/Content/EventScheduleDatePick/EventScheduleDatePick.js
+++ src/Components/Admin/Home/Pages/LiveChatSpStar/Content/EventScheduleDatePick/EventScheduleDatePick.js
@@ -37,25 +37,55 @@ const EventScheduleDatePick = () => {
                 />
 
 
+
             </div>
 
             <div>
                 <div className="d-flex flex-wrap my-5">
                     {dates.map((date, i) => {
                         return (
-                            <div key={i} className="eventDatePicked d-flex">
-                                {/* {console.log(date)} */}
-                                {/* <div className="d-flex"> */}
-                                <div>
-                                    {date.day} {date.month.shortName}
-                                    {console.log(i)}
+                            <div className="m-2">
+                                <div className="cardContainer">
+                                    <div className="face face1">
+                                        <div key={i} className="eventDatePicked d-flex">
+                                            {/* {console.log(date)} */}
+                                            {/* <div className="d-flex"> */}
+                                            <div>
+                                                {date.day} {date.month.shortName}
+                                                {console.log(i)}
+                                            </div>
+                                            <button className="removeDatePick ms-3" onClick={() => handleRemoveClick(i)} type="button"><i className="fas fa-times"></i></button>
+                                            {/* </div> */}
+                                        </div>
+                                    </div>
+
+                                    <div class="face face2">
+                                        <div class="content">
+                                            <h5 className="text-white"><i className="fas fa-calendar-week mx-1"></i> <span className="mx-3">Schedule</span></h5>
+                                            <div className="d-flex">
+                                                <div className="schedule-start-time">
+                                                    4.00 pm
+                                                </div>
+                                                <span>To</span>
+                                                <div className="schedule-end-time mx-2">
+                                                    5.00 pm
+                                                </div>
+                                            </div>
+                                           
+                                            <a href="#">Details</a>
+                                        </div>
+                                    </div>
+
+                                    {/* <div class="face face3">
+                                        <div class="content">
+                                            <h5 className="text-white"><i className="fas fa-calendar-week mx-1 schedule-icon"></i> <span className="mx-3">Schedule</span></h5>
+                                            <p>No Schedule Here</p>
+                                           
+                                            <a href="#">Add New</a>
+                                        </div>
+                                    </div> */}
                                 </div>
-                                <button className="removeDatePick ms-3" onClick={() => handleRemoveClick(i)} type="button"><i className="fas fa-times"></i></button>
-                                {/* </div> */}
-
-
                             </div>
-
                         )
                     }
                     )}
