ff511f24cdf0a0843568a1b42d5ee124dc56643c
Apurbo Kumar Anup <apurboka@gmail.com>
2022-01-24 14:59:06 +0600

    Update Schedule

---
+++ src/Components/Admin/Home/Pages/ScheduleCreate/NewSchedule.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import NewScheduleForm from './NewScheduleForm';
+
+const NewSchedule = () => {
+const [data, setData] = useState([
+<NewScheduleForm />
+])
+
+const singleInput = (
+<NewScheduleForm />
+);
+
+const handleAddClick = () => {
+setData([...data, singleInput]);
+}
+
+return (
+<div className='container-fluid my-2 bg-dark SN-ce'>
+    <div className="d-flex justify-content-between">
+        <h5 className='text-light mt-4 mx-4'>20th January 2022</h5>
+        <hr/>
+    </div>
+    {data.map((single, i) => {
+    return (
+    <>
+        <div className="mt-3">
+
+            <div className="col-md-11 mx-4">
+                <div key={i}>{single}</div>
+                <div>{console.log(single)}</div>
+
+            </div>
+        </div>
+    </>
+    );
+    }
+
+    )}
+        <div className="d-flex mt-4 mb-5 pb-4 justify-content-center">
+            <button type="button" className="btn btn-primary  bntTSC">Save</button>
+            <button onClick={handleAddClick} type="button" className="btn btn-success mx-3 bntTSC "><i class="fas fa-plus"></i> &nbsp;New Schedule</button>
+        </div>
+
+</div>
+);
+};
+
+export default NewSchedule;
