ec011a972e07ba5ffd88755d8722e17f9b97b8cf
Apurbo Kumar Anup <apurboka@gmail.com>
2022-02-02 15:03:36 +0600

    StarVideoChat.css

--- src/Components/SuperStar/Pages/StarVideoChat/StarUserPortal.js
+++ src/Components/SuperStar/Pages/StarVideoChat/StarUserPortal.js
@@ -7,46 +7,97 @@ return (
 
     <div className="my-3 ">
         <div className="bg-dark TabUsers">
-            <div className='row mx-2 '>
+            <div className='row mx-2 TabUsers1'>
 
                 <div className="col-md-6 mt-3">
                     <div className="container-Tab-User d-flex">
                         <img src={person} alt="" className="UserTabImg" />
                         <div className="UserTextTab1"></div>
                         <div className="UserTextTab">Sahib All Hasan</div>
+                    </div>
                 </div>
+
+                <div className="col-md-6 mt-3">
+                    <div className="container-Tab-User d-flex">
+                        <img src={person} alt="" className="UserTabImg" />
+                        <div className="UserTextTab1"></div>
+                        <div className="UserTextTab">Tumpa </div>
+
+                    </div>
+                </div>
+
+                <div className="col-md-6 mt-3">
+                    <div className="container-Tab-User d-flex">
+                        <img src={person} alt="" className="UserTabImg" />
+                        <div className="UserTextTab1"></div>
+                        <div className="UserTextTab">Sharna Shaha</div>
+
+                    </div>
+                </div>
+                <div className="col-md-6 mt-3">
+                    <div className="container-Tab-User d-flex">
+                        <img src={person} alt="" className="UserTabImg" />
+                        <div className="UserTextTab1"></div>
+                        <div className="UserTextTab">Sahib All Hasan</div>
+                    </div>
+                </div>
+
+                <div className="col-md-6 mt-3">
+                    <div className="container-Tab-User d-flex">
+                        <img src={person} alt="" className="UserTabImg" />
+                        <div className="UserTextTab1"></div>
+                        <div className="UserTextTab">Tumpa </div>
+
+                    </div>
+                </div>
+
+                <div className="col-md-6 mt-3">
+                    <div className="container-Tab-User d-flex">
+                        <img src={person} alt="" className="UserTabImg" />
+                        <div className="UserTextTab1"></div>
+                        <div className="UserTextTab">Kajol Roy</div>
+
+                    </div>
+                </div>
+
+                <div className="col-md-6 mt-3">
+                    <div className="container-Tab-User d-flex">
+                        <img src={person} alt="" className="UserTabImg" />
+                        <div className="UserTextTab1"></div>
+                        <div className="UserTextTab">Sahib All Hasan</div>
+                    </div>
+                </div>
+
+                <div className="col-md-6 mt-3">
+                    <div className="container-Tab-User d-flex">
+                        <img src={person} alt="" className="UserTabImg" />
+                        <div className="UserTextTab1"></div>
+                        <div className="UserTextTab">Tumpa </div>
+
+                    </div>
+                </div>
+
+                <div className="col-md-6 mt-3">
+                    <div className="container-Tab-User d-flex">
+                        <img src={person} alt="" className="UserTabImg" />
+                        <div className="UserTextTab1"></div>
+                        <div className="UserTextTab">Sharna Shaha</div>
+
+                    </div>
+                </div>
+
+                <div className="col-md-6 mt-3">
+                    <div className="container-Tab-User d-flex">
+                        <img src={person} alt="" className="UserTabImg" />
+                        <div className="UserTextTab1"></div>
+                        <div className="UserTextTab">Kajol Roy</div>
+
+                    </div>
+                </div>
+
             </div>
 
-            <div className="col-md-6 mt-3">
-                <div className="container-Tab-User d-flex">
-                    <img src={person} alt="" className="UserTabImg" />
-                    <div className="UserTextTab1"></div>
-                    <div className="UserTextTab">Tumpa </div>
-
-                </div>
-            </div>
-
-            <div className="col-md-6 mt-3">
-                <div className="container-Tab-User d-flex">
-                    <img src={person} alt="" className="UserTabImg" />
-                    <div className="UserTextTab1"></div>
-                    <div className="UserTextTab">Sharna Shaha</div>
-
-                </div>
-            </div>
-
-            <div className="col-md-6 mt-3">
-                <div className="container-Tab-User d-flex">
-                    <img src={person} alt="" className="UserTabImg" />
-                    <div className="UserTextTab1"></div>
-                    <div className="UserTextTab">Kajol Roy</div>
-
-                </div>
-            </div>
         </div>
-        <br />
-
-    </div>
     </div>
 </>
 )
