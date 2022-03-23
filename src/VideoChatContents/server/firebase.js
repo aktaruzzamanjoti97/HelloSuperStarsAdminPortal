cbef29730b4afb7bb9385db85c7a7a2b7e49dccd
Md. Shawn Shikder Sonet <75745559+sonet1182@users.noreply.github.com>
2022-03-23 12:11:27 +0600

    Change Firebase URL

--- src/VideoChatContents/server/firebase.js
+++ src/VideoChatContents/server/firebase.js
@@ -6,14 +6,8 @@ import firebase from "firebase";
 // };
 
 var firebaseConfig = {
-<<<<<<< Updated upstream
-  apiKey: "AIzaSyD3xegugXiXeX1W2MTdDorwq-wM4fR25cA", // Add API Key
-  databaseURL:
-    "https://meetcloneapp-default-rtdb.asia-southeast1.firebasedatabase.app/", // Add databaseURL
-=======
   apiKey: "AIzaSyBO1__jCxGrPE8RrfP8i1JTkwRNvkcS4E0", // Add API Key
   databaseURL:"https://hellosup-62016-default-rtdb.asia-southeast1.firebasedatabase.app/" // Add databaseURL
->>>>>>> Stashed changes
 };
 
 
