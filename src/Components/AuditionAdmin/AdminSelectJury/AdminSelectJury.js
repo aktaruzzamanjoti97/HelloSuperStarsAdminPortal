a34484fb586fd5ccf979e2b366e1c720a7ef65f8
Monir Hossen <38731309+MonirHossen@users.noreply.github.com>
2022-04-10 14:01:39 +0600

    marks wise and number wise dynamic selected

--- src/Components/AuditionAdmin/AdminSelectJury/AdminSelectJury.js
+++ src/Components/AuditionAdmin/AdminSelectJury/AdminSelectJury.js
@@ -4,6 +4,14 @@ import JuryMarkingDetails from "./JuryMarkingDetails";
 import "./AdminSelectJury.css";
 import axios from "axios";
 import swal from "sweetalert";
+import { Form } from "react-bootstrap";
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
 
 const AdminSelectJury = (props) => {
   let history = useHistory();
@@ -11,9 +19,11 @@ const AdminSelectJury = (props) => {
   const [participants, setAuditionParticipant] = useState([]);
 
   const [juries, setJuries] = useState([]);
-  const [selectedTop,setSelectedTop] = useState("");
-  const [selectedMessage,setSelectedMessage] = useState("");
-  const [rejectedMessage,setRejecteddMessage] = useState("");
+  const [selectedTop, setSelectedTop] = useState("");
+  const [selectedMessage, setSelectedMessage] = useState("");
+  const [rejectedMessage, setRejecteddMessage] = useState("");
+  const [mark_wise,setMarkWise] = useState("");
+  const [number_wise,setNumberWise] = useState("");
 
   var aud_id = props.match.params.id;
 
@@ -24,34 +34,52 @@ const AdminSelectJury = (props) => {
         if (res.data.status === 200) {
           setAuditionParticipant(res.data.audition_participants);
           setJuries(res.data.juries);
-          console.log('juries',res.data.juries);
+          console.log("juries", res.data.juries);
         }
       });
   }, [aud_id]);
 
   const handleSelectedTop = (e) => {
     setSelectedTop(e.target.value);
-  }
+  };
   const handleSelectedMessage = (e) => {
     setSelectedMessage(e.target.value);
-  }
+  };
   const handleRejectedMessage = (e) => {
     setRejecteddMessage(e.target.value);
+  };
+
+  const handleMark = (e) => {
+    setMarkWise(e.target.value);
+    setNumberWise('');
   }
+
+  const handleNumber = (e) => {
+    setNumberWise(e.target.value);
+    setMarkWise('');
+  }
+
+  console.log('mark_wise',mark_wise);
+  console.log('number_wise',number_wise);
 
   // Submit Selected Video
   const submitSelectedVideo = (e) => {
     e.preventDefault();
     const formData = new FormData();
+    formData.append("audition_id", aud_id);
     formData.append("selected_top", selectedTop);
     formData.append("message", selectedMessage);
+    formData.append("mark_wise", mark_wise);
+    formData.append("number_wise", number_wise);
+
+
     axios.get("/sanctum/csrf-cookie").then((response) => {
       axios
         .post(`api/audition-admin/selected-top-videos`, formData)
         .then((res) => {
           if (res.data.status === 200) {
-            setSelectedTop('');
-            setSelectedMessage('');
+            setSelectedTop("");
+            setSelectedMessage("");
             swal("Success", res.data.message, "success");
           }
         });
@@ -68,7 +96,7 @@ const AdminSelectJury = (props) => {
         .post(`api/audition-admin/rejected-videos-message`, formData)
         .then((res) => {
           if (res.data.status === 200) {
-            setRejecteddMessage('');
+            setRejecteddMessage("");
             swal("Success", res.data.message, "success");
             history.push(`/audition-admin/auditions/round2-appeal/${aud_id}`);
           }
@@ -76,13 +104,10 @@ const AdminSelectJury = (props) => {
     });
   };
 
-  
-
-
   return (
     <div className="container">
       <div className="row mb-5">
-        {juries?.map((jury,i) => (
+        {juries?.map((jury, i) => (
           <div className="col-md-4 my-3 d-flex justify-content-center">
             <div className="bg-dark cardBox">
               <div className="px-3 d-flex justify-content-start nameBg rounded-3">
@@ -96,7 +121,7 @@ const AdminSelectJury = (props) => {
                 </div>
               </div>
 
-              <JuryMarkingDetails juryId={jury.id}/>
+              <JuryMarkingDetails juryId={jury.id} />
 
               <div className="m-3">
                 <button className="w-100 btn nameBg p-2">
@@ -117,7 +142,32 @@ const AdminSelectJury = (props) => {
           <p className="text-light">
             <big>Select top</big>
           </p>
-          <div className="col-md-3">
+          <div className="col-md-3 radioButtonSelect">
+            <FormControl>
+       
+              <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="row-radio-buttons-group"
+              >
+                <FormControlLabel
+
+                  value="mark"
+                  control={<Radio />}
+                  label="Mark"
+                  onChange={handleMark}
+                />
+                <FormControlLabel
+                  value="number"
+                  control={<Radio />}
+                  label="Number"
+                  onChange={handleNumber}
+                />
+            
+              </RadioGroup>
+            </FormControl>
+
+            
             <input
               type="text"
               className="my-2 form-control input-gray border border-warning"
@@ -146,7 +196,12 @@ const AdminSelectJury = (props) => {
 
         <div className="row p-4">
           <div className="col-md-6">
-            <span className="btn nameBg fw-bold w-100" onClick={submitSelectedVideo}>Send</span>
+            <span
+              className="btn nameBg fw-bold w-100"
+              onClick={submitSelectedVideo}
+            >
+              Send
+            </span>
           </div>
         </div>
       </div>
@@ -171,7 +226,12 @@ const AdminSelectJury = (props) => {
 
           <div className="row p-4">
             <div className="col-md-4">
-              <span className="btn nameBg fw-bold w-100" onClick={submitRejectedVideo}>Send</span>
+              <span
+                className="btn nameBg fw-bold w-100"
+                onClick={submitRejectedVideo}
+              >
+                Send
+              </span>
             </div>
           </div>
         </div>
