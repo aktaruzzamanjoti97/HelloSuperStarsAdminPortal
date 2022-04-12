35bebf55b22346c264a263ad03f157aef7b581b6
Apurbo Kumar Anup <apurboka@gmail.com>
2022-04-12 13:54:10 +0600

    Video Done

--- src/Components/Admin/FanbaseGroup/FanBaseComponents/MediaComponent/FanBaseMedia.js
+++ src/Components/Admin/FanbaseGroup/FanBaseComponents/MediaComponent/FanBaseMedia.js
@@ -10,6 +10,7 @@ import img7 from '.././../../../../assets/images/Fanbase-img/7.png';
 import img8 from '.././../../../../assets/images/Fanbase-img/8.png';
 import img10 from '.././../../../../assets/images/Fanbase-img/10.png';
 import axios from 'axios';
+import ReactPlayer from "react-player";
 import { useParams } from 'react-router-dom';
 const mediaData = [img1, img2, img3, img4, img5, img6, img7, img8, img10, img1, img2, img3];
 
@@ -27,9 +28,10 @@ const FanBaseMedia = () => {
   }, [slug]);
 
   return (
+    <>
     <div className='card my-3 bg-dark'>
       <div className="card-boy">
-        <h4 className='text-light p-3'>Media Files</h4>
+        <h4 className='text-light p-3'>Photos Files</h4>
         <div className="container p-3">
           <div className="row">
             {fanMedia.map((post, i) => {
@@ -39,14 +41,35 @@ const FanBaseMedia = () => {
                 </div>
               </div>
             })}
-
-
-
           </div>
 
         </div>
       </div>
     </div>
+
+    <div className='card my-3 bg-dark'>
+      <div className="card-boy">
+        <h4 className='text-light p-3'>Videos Files</h4>
+        <div className="container p-3">
+          <div className="row">
+            {fanMedia.map((post, i) => {
+              return <div className="col-3 mb-4" >
+                <div className="content-1" style={{ border: '2px solid gold', borderRadius: '10px', cursor: 'pointer' }}>
+
+                <ReactPlayer className='GdsAS'
+                  url="https://www.youtube.com/watch?v=LRtEJPSj2-8"
+                  autoplay
+                  controls="true"
+                />
+                </div>
+              </div>
+            })}
+          </div>
+
+        </div>
+      </div>
+    </div>
+    </>
   );
 };
 
